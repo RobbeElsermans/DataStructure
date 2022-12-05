9b34481c6ebeb20c9a9b8848de1c621ea5e44653
Robbe Elsermans <robels@outlook.com>
2022-12-05 18:02:30 +0100

    add Production

---
+++ src/LSystem/Validation.cpp
@@ -0,0 +1,15 @@
+//
+// Created by robbeelsermans on 12/5/22.
+//
+
+#include "LSystem/Validation.h"
+
+/*
+template<typename SymbolType>
+bool isValidProduction(
+        const Production<SymbolType>& production,
+        const std::unordered_set<SymbolType>& alphabet
+)
+{
+
+}*/
