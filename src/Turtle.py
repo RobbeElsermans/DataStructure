d8c72f353888de8fb0b2bee0b5198bb63976d297
Robbe Elsermans <robels@outlook.com>
2022-12-12 17:40:44 +0100

    add couple of drawings

--- src/Turtle.py
+++ src/Turtle.py
@@ -1,29 +1,65 @@
-#/bin/python
-
+# /bin/python
+import sys
 import turtle
 import fileinput
 
-turtle.speed("fastest")
-text = fileinput.input("../output/test1670786310.txt").readline();
 
-print(text)
+def draw(text, forward, left, right):
+    stack = []
+    for command in text:
+        turtle.pd()
+        if command in ["F", "G", "R", "L"]:
+            turtle.forward(forward)
+        elif command == "f":
+            turtle.pu()  # pen up - not drawing
+            turtle.forward(forward)
+        elif command == "+":
+            turtle.right(right)
+        elif command == "-":
+            turtle.left(left)
+        elif command == "[":
+            stack.append((turtle.position(), turtle.heading()))
+        elif command == "]":
+            turtle.pu()  # pen up - not drawing
+            position, heading = stack.pop()
+            turtle.goto(position)
+            turtle.setheading(heading)
 
-for item in text:
-    print(item)
+    turtle.done()
 
-    #if item == 'X' or item == 'Y':
-        #print("do nothing")
+def main():
+    turtle.speed(0)  # het snelste
+    turtle_screen = turtle.Screen()
+    turtle_screen.screensize(2000, 2000)
+    text = fileinput.input("../output/testTree1670862632.txt").readline()
 
-    if item == 'F':
-        #print("go forward")
-        turtle.forward(5)
+    turtle.left(90)
 
-    if item == '+':
-        #print("go 90° right")
-        turtle.right(100)
+    print(text)
+    # 0 -> triangle
+    # 1 -> plant
+    # 2 -> hilbert
+    # 3 -> dragon
+    # 4 -> snow
+    # 5 -> tree
+    typen = 5
 
-    if item == '-':
-        #print("go 90° left")
-        turtle.left(38)
+    if typen == 0:
+        draw(text, 15, 80, 80)
+    elif typen == 1:
+        draw(text, 5, 25, 25)
+    elif typen == 2:
+        draw(text, 15, 90, 90)
+    elif typen == 3:
+        draw(text, 5, 25, 25)
+    elif typen == 4:
+        draw(text, 5, 90, 90)
+    elif typen == 5:
+        draw(text, 5, 22.5, 22.5)
 
-turtle.done()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except BaseException:
+        sys.exit(0)
--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,132 @@
 #include <ctime>
 #include "../lib/L_System/include/LSystemInterpreter.hpp"
 
+typedef enum Type{
+    triangle,
+    plant,
+    dragon,
+    hilbert,
+    snowWindow,
+    tree,
+};
+
+void createTriangle();
+void createPlant();
+void createDragon();
+void createHilbert();
+void createSnowWindow();
+
+void CreateFile(std::vector<char> &getValue, const std::string& fileExtention);
+
+void createTree();
+
 int main() {
     //std::cout << "Hello, World!" << std::endl;
+    Type type = tree;
 
-    //https://ibeach.github.io/turtle/
-    const std::vector<char> axiom{'F','-','F','-','F','-','F'};
+    switch (type) {
+        case triangle:
+            createTriangle();
+            break;
+        case plant:
+            createPlant();
+            break;
+        case hilbert:
+            createHilbert();
+            break;
+        case snowWindow:
+            createSnowWindow();
+            break;
+        case tree:
+            createTree();
+            break;
+    }
+
+    return 0;
+}
+
+void createTree() {
+//https://ibeach.github.io/turtle/
+    const std::vector<char> axiom{'F'};
     const std::unordered_set<Production<char>> productions{
-            Production<char>{'F', {'F', '+', 'F', 'F', '-'}}
-            //Production<char>{'Y', {'-', 'F', 'X', '-', 'Y'}},
+            Production<char>{'F', {'F','F', '-', '[', '-', 'F','+','F','+','F',']','+','[','+','F','-','F','-','F',']'}},
     };
 
-    const std::unordered_set<char> alphabet{'X','Y','F','+','-'};
+    const std::unordered_set<char> alphabet{'F','+','-','[',']'};
 
-    //Drawing
-    // F    means go forward by 20 px
-    // -    means go left by 90°
-    // +    means go right by 90°
-    // X, Y means do nothing
+    std::vector<char> getValue = LSystemInterpreter<char>(axiom, productions, alphabet).generate(4);
 
-    std::vector<char> getValue = LSystemInterpreter<char>(axiom, productions, alphabet).generate(13);
+    CreateFile(getValue, "Tree");
+}
 
-    //output getValue to file with timestamp
-    std::ofstream saveFile("../output/test"+std::to_string(std::time(0))+".txt", std::ios::out);
+void createTriangle(){
+    //https://ibeach.github.io/turtle/
+    const std::vector<char> axiom{'L'};
+    const std::unordered_set<Production<char>> productions{
+            Production<char>{'L', {'[','R', '-', 'L', '-', 'R',']'}},
+            Production<char>{'R', {'L', '+', 'R', '+', 'L'}},
+    };
+
+    const std::unordered_set<char> alphabet{'R','L','+','-','[',']'};
+
+    std::vector<char> getValue = LSystemInterpreter<char>(axiom, productions, alphabet).generate(10);
+
+    CreateFile(getValue, "Triangle");
+}
+
+void createPlant(){
+    //https://ibeach.github.io/turtle/
+    const std::vector<char> axiom{'X'};
+    const std::unordered_set<Production<char>> productions{
+            //X → F+[[X]-X]-F[-FX]+X
+            Production<char>{'X', {'F', '+', '[', '[', 'X', ']', '-', 'X', ']', '-', 'F', '[', '-', 'F','X', ']', '+','X'}},
+            //F → FF
+            Production<char>{'F', {'F', 'F'}},
+    };
+
+    const std::unordered_set<char> alphabet{'F','X','+','-','[',']'};
+
+    std::vector<char> getValue = LSystemInterpreter<char>(axiom, productions, alphabet).generate(6);
+
+    CreateFile(getValue, "Plant");
+}
+
+void createHilbert(){
+    //https://ibeach.github.io/turtle/
+    const std::vector<char> axiom{'A'};
+    const std::unordered_set<Production<char>> productions{
+            //X → F+[[X]-X]-F[-FX]+X
+            Production<char>{'A', {'+','B','F','-','A','F','A','-','F','B','+'}},
+            //F → FF
+            Production<char>{'B', {'-','A','F','+','B','F','B','+','F','A','-'}},
+    };
+
+    const std::unordered_set<char> alphabet{'A','B','F','-','+'};
+
+    std::vector<char> getValue = LSystemInterpreter<char>(axiom, productions, alphabet).generate(6);
+
+    CreateFile(getValue, "Hilbert");
+}
+
+void createSnowWindow(){
+    //https://ibeach.github.io/turtle/
+    const std::vector<char> axiom{'F','-','F','-','F', '-', 'F'};
+    const std::unordered_set<Production<char>> productions{
+            Production<char>{'F', {'F','F','-','F','-','-','F','-','F'}}
+    };
+
+    const std::unordered_set<char> alphabet{'A','B','F','-','+'};
+
+    std::vector<char> getValue = LSystemInterpreter<char>(axiom, productions, alphabet).generate(4);
+
+    CreateFile(getValue, "SnowWindow");
+}
+
+void CreateFile(std::vector<char> &getValue, const std::string& fileExtention) {//output getValue to file with timestamp
+    std::ofstream saveFile("../output/test"+fileExtention + std::to_string(time(0)) + ".txt", std::ios::out);
 
     for(char c : getValue)
-    saveFile << c;
+        saveFile << c;
 
     saveFile.close();
-    return 0;
 }
